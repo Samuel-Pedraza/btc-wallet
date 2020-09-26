de41adbdd74bb869f06d78d8f1372c3e24720fd7
unknown <samdpedraza@gmail.com>
2020-09-26 15:59:12 -0700

    can create a 'wallet'

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 const mysql = require('mysql');
 const net = require('net');
 
+let win;
 
 function createWindow(){
-	const win = new BrowserWindow({
+	win = new BrowserWindow({
 		width: 800,
 		height: 800,
 		webPreferences: {
@@ -22,10 +23,24 @@ let connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
 	password: 'password',
-	database: 'bitcoind'
+	database: 'bitcoindb'
 })
 
 
+connection.connect(function(err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+});
+
 ipcMain.on('wallet:create', (e, options) => {
 	console.log(options);
+
+	let sql = 'INSERT INTO wallet(wallet_name) VALUES("' + options.wallet + '")';
+	console.log(sql);
+	connection.query(sql);
+	connection.end();
+
+	win.webContents.send('wallet:done');
 });
+
